554aa62d2031e47314272bc353644799dc2e7c8d
init.defaultBranch <benlimpic@gmail.com>
2025-03-04 13:00:30 -0800

    error handling signup

--- src/main/java/com/authentication/demo/Model/MyAppUser.java
+++ src/main/java/com/authentication/demo/Model/MyAppUser.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +16,10 @@ public class MyAppUser {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  @Column(unique = true)
   private String username;
+
   private String email;
   private String password;
 }
