cf42011b3e66957f49d76f90df518c2c1ef34fa2
init.defaultBranch <benlimpic@gmail.com>
2025-03-21 17:42:53 -0700

    formatting update page

---
+++ src/main/resources/static/js/updateChanges.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+const usernameInput = document.getElementById("usernameInput");
+const locationInput = document.getElementById("locationInput");
+const biographyInput = document.getElementById("biographyInput");
+const emailInput = document.getElementById("emailInput");
+const websiteInput = document.getElementById("websiteInput");
+const saveChangesButton = document.getElementById("saveChangesButton");
+
+
+
+});
