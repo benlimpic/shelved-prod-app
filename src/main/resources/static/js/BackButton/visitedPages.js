9647c2481a789182ed2932490d2116b68f8c0fcd
init.defaultBranch <benlimpic@gmail.com>
2025-04-29 09:25:12 -0700

    semi working backward navigation

---
+++ src/main/resources/static/js/BackButton/visitedPages.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // CONSTRUCTOR FOR PAGE ITEM
+  class PageItem {
+    constructor(url, scrollY, isBack) {
+      this.url = url;
+      this.scrollY = scrollY;
+      this.isBack = isBack;
+    }
+  }
+
+  // GETTING VISITED PAGES FROM SESSION STORAGE
+  let visitedPages = JSON.parse(sessionStorage.getItem('visitedUrls')) || [];
+
+  // ADDING CURRENT PAGE TO VISITED PAGES
+  const currentUrl = window.location.href;
+
+  
+
+  // Check if the current page is already in the visited pages
+  if (!visitedPages.some((page) => page.url === currentUrl && page.isBack)) {
+    const pageItem = new PageItem(currentUrl, window.scrollY, false); // isBack = false for normal navigation
+    visitedPages.push(pageItem);
+    sessionStorage.setItem('visitedUrls', JSON.stringify(visitedPages));
+    console.log('Added new page to visited pages:', pageItem);
+  } else {
+    console.log('Page is marked as isBack and will not be added:', currentUrl);
+  }
+
+  // BACK BUTTON FUNCTIONALITY
+  const backButton = document.getElementById('backButton');
+  if (backButton) {
+    backButton.addEventListener('click', function () {
+      if (visitedPages.length > 1) {
+        // Remove the current page
+        visitedPages.pop();
+
+        // Get the last visited page
+        const lastVisitedPage = visitedPages[visitedPages.length - 1];
+        sessionStorage.setItem('visitedUrls', JSON.stringify(visitedPages));
+
+        // Mark the last visited page as isBack = true
+        lastVisitedPage.isBack = true;
+        console.log('Navigating back to:', lastVisitedPage);
+
+        // Navigate to the last visited page
+        window.location.href = lastVisitedPage.url;
+      } else {
+        console.log('No previous pages to go back to.');
+      }
+    });
+  }
+
+  console.log('Visited Pages:', visitedPages);
+
+  // SAVE SCROLL POSITION ON BEFOREUNLOAD
+  window.addEventListener('beforeunload', function () {
+    const currentScrollY = window.scrollY;
+
+    // Update the scrollY value for the current page in visitedPages
+    const currentPage = visitedPages.find((page) => page.url === currentUrl);
+    if (currentPage) {
+      currentPage.scrollY = currentScrollY;
+      sessionStorage.setItem('visitedUrls', JSON.stringify(visitedPages));
+      console.log('Updated scroll position for current page:', currentPage);
+    }
+  });
+
+  // RESTORE SCROLL POSITION ON PAGE LOAD
+  window.addEventListener('load', function () {
+    const currentPage = visitedPages.find((page) => page.url === currentUrl);
+    if (currentPage && currentPage.scrollY !== undefined) {
+      window.scrollTo(0, currentPage.scrollY);
+      console.log('Restored scroll position to:', currentPage.scrollY);
+    }
+  });
+});
--- src/main/resources/static/js/visitedPages.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let visitedUrls = JSON.parse(sessionStorage.getItem("visitedUrls")) || [];
-  if (visitedUrls[visitedUrls.length - 1] !== window.location.href) {
-    visitedUrls.push(window.location.href);
-    sessionStorage.setItem("visitedUrls", JSON.stringify(visitedUrls));
-  }
-});
