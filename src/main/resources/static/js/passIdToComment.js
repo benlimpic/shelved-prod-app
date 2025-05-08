1f9cd409557797945f206a6d976da5a7f0a2bbb5
init.defaultBranch <benlimpic@gmail.com>
2025-05-08 14:36:29 -0700

    collection replies to replies working

--- src/main/resources/static/js/passIdToComment.js
+++ src/main/resources/static/js/passIdToComment.js
@@ -24,14 +24,75 @@ const triggerCommentId = (button) => {
   }
 
   if (commentActivity && commentReplyActivity) {
-    if (!commentActivity.classList.contains('hidden')) {
       commentActivity.classList.add('hidden');
       commentReplyActivity.classList.remove('hidden');
-    } else {
-      commentActivity.classList.remove('hidden');
-      commentReplyActivity.classList.add('hidden');
-    }
-  } else {
+  }
+  else {
     console.error("Comment activity or reply activity element not found.");
   }
 };
+
+
+
+const triggerReplyId = (button) => {
+
+if (!button) {
+    console.error("Button element is null or undefined.");
+    return;
+  }
+
+  const commentId = button.getAttribute('data-comment-id');
+  if (!commentId) {
+    console.error("Button does not have a 'data-comment-id' attribute.");
+    return;
+  }
+
+  let usernameSlug = "";
+
+  const username = button.getAttribute('data-reply-username');
+  if (!username || username.trim() === "") {
+    usernameSlug = "";
+    console.error("Button does not have a 'data-reply-username' attribute or it is empty.");
+    return;
+  }
+  usernameSlug = "@" + username + " ";
+  
+
+  const commentReplyActivity = document.getElementById('commentReplyActivity');
+  const commentActivity = document.getElementById('commentActivity');
+  const commentIdInput = document.getElementById('replyCommentIdInput');
+  const replyContentInput = document.getElementById('replyContentInput');
+
+
+
+
+  if (commentIdInput) {
+    commentIdInput.setAttribute('value', commentId);
+    console.log(`Hidden input value set to: ${commentId}`);
+  } else {
+    console.error("Hidden input with ID 'replyCommentIdInput' not found.");
+  }
+
+
+  if (!replyContentInput) {
+    console.error("Hidden input with ID 'replyContentInput' not found.");
+    return;
+  }
+
+  if (replyContentInput) {
+    const currentValue = replyContentInput.value;
+    const newValue = usernameSlug;
+    replyContentInput.setAttribute('value', newValue);
+    console.log(`Hidden input value set to: ${newValue}`);
+  }
+
+  if (commentActivity && commentReplyActivity) {
+    commentActivity.classList.add('hidden');
+    commentReplyActivity.classList.remove('hidden');
+  }
+  else {
+    console.error("Comment activity or reply activity element not found.");
+  }
+
+};
+
